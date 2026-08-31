21a4a6667893ba67d41c8d95efec7b55b7d15137
zainulabidin776 <itsmezayynn@gmail.com>
2026-08-31 20:58:14 +0500

    chore: initialize frontend and backend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">
+        Notifications System
+      </h1>
+    </div>
+  );
+}
+
+export default App;
