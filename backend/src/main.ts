2467922dd0446eb2d1649735016d4fa799b9be0b
zainulabidin776 <itsmezayynn@gmail.com>
2026-09-01 13:21:18 +0500

    fix: enable cors for frontend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,14 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:5173',
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
